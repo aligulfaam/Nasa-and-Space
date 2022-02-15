c14a90a0dc497db8643934cd8044a6282debde65
aligulfaamstudent <90401861+aligulfaamstudent@users.noreply.github.com>
2022-02-15 13:10:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+var Question1;
+var Question2;
+var Question3;
+var Question4;
+
+//Question 1
+function correctAnswer1(){
+    Question1 = correctAnswer1;
+switch(true){
+    case correctAnswer1 = incorrect:
+        document.getElementById("triviaMessage1a").innerHTML = "Incorrect!";
+    default:
+        document.getElementById("triviaMessage1a").innerHTML = "Correct!";
+        document.getElementById("score").innerHTML = "1/4"
+        document.getElementById("correct").style = "green";
+    }
+}
+
+//Question 2
+function correctAnswer2(){
+    Question2 = correctAnswer2;
+    
+switch(true){
+    case Question2 = "incorrect":
+        document.getElementById("triviaMessage2b").innerHTML = "Incorrect!";
+        break;
+    default:
+        document.getElementById("triviaMessage2b").innerHTML = "Correct!";
+        document.getElementById("score").innerHTML = "2/4";
+}
+}
+
+    //Question 3
+function correctAnswer3(){
+    if (correctAnswer3 = "correct"){
+        document.getElementById("triviaMessage3c").innerHTML = "Correct!";
+        document.getElementById("score").innerHTML = "3/4";
+    } else { (Question4 == incorrect) 
+        document.getElementById("triviaMessage3c").innerHTML = "Incorrect!";
+    }
+}
+
+
+    //Question 4
+function correctAnswer4(){
+    if (correctAnswer4 = correct){
+        document.getElementById("triviaMessage4d").innerHTML = "Correct!";
+        document.getElementById("score").innerHTML = "4/4";
+    } else {
+        document.getElementById("triviaMessage4d").innerHTML = "Incorrect!";
+    }
+}
